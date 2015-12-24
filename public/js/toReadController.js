e37b4fb5fc379d7ba90081e51b3503be07b45ca3
Noah Springwater <nspringwater@gmail.com>
2015-12-24 06:43:18 -0500

    controller work, adding functions

--- public/js/toReadController.js
+++ public/js/toReadController.js
@@ -8,12 +8,47 @@ function readMeAppCtrl ($http, $log) {
   self.heading = "ReadMe";
 
 
+  self.allToRead;
+  self.newToRead = {};
+  self.addToRead = addToRead;
+  self.getToRead;
+  self.deleteToRead = deleteToRead;
+  getToRead();
+
+  self.allAlreadyRead;
+  self.newAlreadyRead = {};
+  self.addAlreadyRead = addAlreadyRead;
+  self.getAlreadyRead;
+  self.deleteAlreadyRead = deleteAlreadyRead;
+  getAlreadyRead();
 
 
+  function getToRead() {
+    //go to data service
+    $http
+      .get('/toRead')
+      .then(function (res){
+        self.allRead = res.data;
+      })
+      .catch(function (res) {
+        $log.error('failure', res)
+      });
+  }
 
-function getToReads() {
-  //go to data service
-  $http
-    .get('/')
-}
+  function getAlreadyRead() {
+    $http
+      .get('/alreadyRead')
+      .then(function (res) {
+        self.allAlreadyRead = res.data;
+      })
+      .catch(function (res) {
+        $log.error('failure', res);
+      });
+  }
+
+  function addToRead() {
+    $http
+      .post('/toRead', self.newBook)
+  }
+
 }
